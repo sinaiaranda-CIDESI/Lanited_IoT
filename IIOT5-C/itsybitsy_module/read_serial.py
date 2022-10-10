966e9f8145da68925a2d845a9cfbe44002c73d7c
arandsi1 <sinai.aranda@cidesi.edu.mx>
2022-10-10 09:59:50 -0500

    IoT 5 topics changes

--- IIOT5-C/itsybitsy_module/read_serial.py
+++ IIOT5-C/itsybitsy_module/read_serial.py
@@ -12,6 +12,12 @@ def read_data():
         data_set = {
             'TEMPERATURE': {'VALUE' : data ['temperature'], 'TIMESTAMP' : str(datetime.now().isoformat())}, 
             'HUMIDITY': {'VALUE' : data ['humidity'], 'TIMESTAMP' : str(datetime.now().isoformat())}
+            'VOLTAGE': {'VALUE' : data ['voltage'], 'TIMESTAMP' : str(datetime.now().isoformat())}, 
+            'CURRENT_1': {'VALUE' : data ['current1'], 'TIMESTAMP' : str(datetime.now().isoformat())}
+            'CURRENT_2': {'VALUE' : data ['current2'], 'TIMESTAMP' : str(datetime.now().isoformat())}, 
+            'CURRENT_3': {'VALUE' : data ['current3'], 'TIMESTAMP' : str(datetime.now().isoformat())}
+            'TOTAL_CURRENT': {'VALUE' : data ['total_current'], 'TIMESTAMP' : str(datetime.now().isoformat())}, 
+            'POWER': {'VALUE' : data ['power'], 'TIMESTAMP' : str(datetime.now().isoformat())}
             }
     except:
         t = str(datetime.now().isoformat())
@@ -19,6 +25,12 @@ def read_data():
         data_set = {
             'TEMPERATURE': {'VALUE' : 0, 'TIMESTAMP' : t}, 
             'HUMIDITY': {'VALUE' : 0, 'TIMESTAMP' : t}
+            'VOLTAGE': {'VALUE' : 0, 'TIMESTAMP' : t}, 
+            'CURRENT_1': {'VALUE' : 0, 'TIMESTAMP' : t}
+            'CURRENT_2': {'VALUE' : 0, 'TIMESTAMP' : t}, 
+            'CURRENT_3': {'VALUE' : 0, 'TIMESTAMP' : t}
+            'TOTAL_CURRENT': {'VALUE' : 0, 'TIMESTAMP' : t}, 
+            'POWER': {'VALUE' : 0, 'TIMESTAMP' : t}
             }
             
     return data_set
