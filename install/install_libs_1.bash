6ab70cb149cd5fb47762d63a4a09ae9877671b60
arandsi1 <sinai.aranda@cidesi.edu.mx>
2022-11-18 12:33:59 -0600

    Final version

---
+++ install/install_libs_1.bash
@@ -0,0 +1,7 @@
+ip addr show
+
+sudo apt-get update
+sudo apt update
+apt list --upgradable
+
+sudo reboot
---
+++ install/install_libs_2.bash
@@ -0,0 +1,9 @@
+sudo apt-get upgrade
+
+sudo apt install python3-pip
+
+pip install pyserial
+pip install minimalmodbus
+pip install paho-mqtt
+
+sudo systemctl set-default multi-user.target
